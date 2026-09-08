47a863b9abe38f6f47601f7872e5596700bb61c8
shashank <shashankpulipatiofl@gmail.com>
2026-09-09 00:43:49 +0530

    chore: migrate from .eslintrc.json to eslint.config.js (Flat Config)

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+];
